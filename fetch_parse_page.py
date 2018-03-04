84a99a3e407ae36dd422990a998412c759e656d0
Vikram <vikr4m.kohli@gmail.com>
2018-03-04 15:58:36 -0600

    Word bank-based tools finder works better... ugh but implemented]

--- fetch_parse_page.py
+++ fetch_parse_page.py
@@ -31,16 +31,10 @@ https://www.allrecipes.com/recipe/12804/seared-scallops-with-spicy-papaya-sauce/
 Labeled as a 'gourmet'/complex recipe, so maybe something weird will happen? (':
 Nope. It was getting confused about verb past participles but that was fixed fairly fast
 
-Tool parser testing;
+Tool parser testing; initially for the tool parser tried an approach that made use of part-of-word, but it was running into some--
+--issues, so eventually just settled for a simpler approach that searches from a list of already-known tool words. Will just keep--
+--updating this to make it more extensive and applicable to a variety of recipes.
 
-https://www.allrecipes.com/recipe/12804/seared-scallops-with-spicy-papaya-sauce/
-Used this one initially; works OK. Came up with a pretty decent list of initial ban words, and the method seems to be OK?
-
-https://www.allrecipes.com/recipe/11901/to-die-for-fettuccini-alfredo/
-This one's annoying; the person spelled fettucine two different ways, so the method can't match the two.
-Added a method for accounting for typos. Maybe should write a simple inference function that adds intuitive tools necessary?
-
-https://www.allrecipes.com/recipe/11116/cream-cheese-sugar-cookies/
 
 """
 
@@ -50,7 +44,7 @@ from nltk import pos_tag, word_tokenize
 
 #   For now, the URL has to be manually changed here
 #   Ideally by the end we'll have some walkthrough / user input interface which will be nicer
-set_url = "https://www.allrecipes.com/recipe/11116/cream-cheese-sugar-cookies/"
+set_url = "https://www.allrecipes.com/recipe/100506/irish-potato-farls/"
 
 def fetch_page(link):
     with urllib.request.urlopen(link) as url:
@@ -90,10 +84,9 @@ def parts_fix(tuples):
     time_units = ['second', 'minute', 'hour']
 
     #  Corrections for the NLTK part-of-speech tagger; can just update this while testing on various recipes
-    JJ_corrections = ['small', 'medium', 'large', 'brown']
-    VB_corrections = ['combine', 'coat', 'cook', 'stir', 'drain', 'toss', 'serve', 'place', 'heat', 'brush', 'beat', 'bake',
-                      'mix', 'cut']
+    JJ_corrections = ['small', 'medium', 'large']
     VBD_corrections = ['ground']
+    VB_corrections = []
     NN_corrections = []
     #  was thinking of puting some stuff like 'extra' / 'to taste' as numbers, but what about like... 'extra-virgin olive oil'
     #  something to consider, I guess
@@ -173,35 +166,35 @@ def parse_ingredient(description):
         i = i + 1
     return ing_data
 
-def parse_tools(instruction, ingredients, title):  
-    #  this actually seemed to improve the identification?? not sure why but will leave it for now
-    lowercase = instruction.lower()
+def parse_tools(instruction):
+    #  banning some words that slip through the cracks
+    banned_words = ['potato']
     
-    # banned words; method is almost perfect, but some common words slip through, so just get rid of them here
-    banned_words = ['heat', 'sauce', 'degree', 'shape', 'place', 'time', 'sprinkle', 'back', 'amount', 'direction',
-                    'dough']
+    #  doing this the old way seemed to not be so great; so I think I'll just keep a running list of tool words instead
+    tool_words = ['pan', 'skillet', 'pot', 'sheet', 'grate', 'whisk', 'spoon', 'cutter', 'board', 'oven', 'bowl', 'bag',
+                  'towel', 'pin', 'knife', 'masher']
     
-    #  create tokens and assign parts-of-word
-    tokens = word_tokenize(lowercase)
-    parts_tuples = pos_tag(tokens)
-    parts = parts_fix(parts_tuples)
+    #  since we're just searching for words, we shouldn't need part-of-word information, just tokens
+    tokens = word_tokenize(instruction)
     
-    # remove all non-noun words
-    parts = [x for x in parts if (x[1] == 'NN' or x[1] == 'NNS') and x[0][-4:] != "ness"]
-    title_words = word_tokenize(title)
-    for title_word in title_words:
-        parts = [x for x in parts if title_word not in x[0] and not misspelling(x[0], title_word)]
-    for banned_word in banned_words:
-        parts = [x for x in parts if banned_word not in x[0]]
-    for ingredient in ingredients:
-        parts = [x for x in parts if x[0] != ingredient and not misspelling(x[0], ingredient)] 
-    if len(parts) < 1:
-        return
-    else:
-        found_tools = []
-        for i in range(0, len(parts)):
-            found_tools.append(parts[i][0])
-        return found_tools
+    #  keep all words with tool words in them (including subsets; e.g. 'saucepan')
+    potential_tools = []
+    for token in tokens:
+        for tool_word in tool_words:
+            if tool_word in token:
+                potential_tools.append(token)
+                break
+    found_tools = []
+    for potential_tool in potential_tools:
+        is_tool = True
+        for banned_word in banned_words:
+            if banned_word in potential_tool:
+                is_tool = False
+                break
+        if is_tool:
+            found_tools.append(potential_tool)
+    return found_tools
+
 
 def full_ingredients_list(ingredients):
     all_ingredients = []
@@ -217,6 +210,14 @@ def misspelling(string1, string2):  # allowing 2-letter difference, for leniency
             mistakes = mistakes + 1
     return mistakes <= 2
 
+def remove_plurals(tools):      # if we have 'spoon' and 'spoons', keep 'spoon' only
+    for i in range(0, len(tools)):
+        for j in range(i, len(tools)):
+            if tools[i] == tools[j] + 's':
+                tools[i] = tools[j]
+            elif tools[j] == tools[i] + 's':
+                tools[j] = tools[i]
+    
 if __name__ == '__main__':
     all_strings = fetch_page(set_url)
     ing_strings = all_strings[0]
@@ -230,10 +231,9 @@ if __name__ == '__main__':
     tools = []
     for dir_string in dir_strings:
         print(dir_string)
-        parsed_tools = parse_tools(dir_string, all_ingredients, title)
-        if parsed_tools != None:
-            tools = tools + [x for x in parsed_tools if x != None]
-            tools = list(set(tools))
+        tools = tools + parse_tools(dir_string)
+    remove_plurals(tools)
+    tools = list(set(tools))    # remove duplicates
     print(tools)
     
     
